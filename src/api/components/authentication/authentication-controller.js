6731a58da71d7bc1dc3070745addc5e861af6706
jamesadrn <jamesandrn@gmail.com>
2024-05-06 15:33:39 +0700

    a

--- src/api/components/authentication/authentication-controller.js
+++ src/api/components/authentication/authentication-controller.js
@@ -1,30 +1,40 @@
+// authentication-controller.js
 const { errorResponder, errorTypes } = require('../../../core/errors');
 const authenticationServices = require('./authentication-service');
 
-/**
- * Handle login request
- * @param {object} request - Express request object
- * @param {object} response - Express response object
- * @param {object} next - Express route middlewares
- * @returns {object} Response object or pass an error to the next route
- */
+const MAX_LOGIN_ATTEMPTS = 5;
+
 async function login(request, response, next) {
   const { email, password } = request.body;
 
   try {
-    // Check login credentials
     const loginSuccess = await authenticationServices.checkLoginCredentials(
       email,
       password
     );
 
     if (!loginSuccess) {
+      const dataPercobaan =
+        await authenticationServices.checkDataPercobaan(email);
+      if (dataPercobaan === MAX_LOGIN_ATTEMPTS) {
+        throw errorResponder(
+          errorTypes.INVALID_CREDENTIALS,
+          `User ${email} gagal login. Attempt: ${dataPercobaan}. LIMIT REACHED!`
+        );
+      }
       throw errorResponder(
         errorTypes.INVALID_CREDENTIALS,
-        'Wrong email or password'
+        `User ${email} gagal login. Attempt : ${dataPercobaan}`
       );
     }
 
+    if (loginSuccess.attempts >= 6) {
+      const menitTunggu = loginSuccess.reset;
+      throw errorResponder(
+        errorTypes.FORBIDDEN,
+        `Too many attempts, try again in ${menitTunggu} minutes`
+      );
+    }
     return response.status(200).json(loginSuccess);
   } catch (error) {
     return next(error);
--- src/api/components/authentication/authentication-service.js
+++ src/api/components/authentication/authentication-service.js
@@ -2,37 +2,73 @@ const authenticationRepository = require('./authentication-repository');
 const { generateToken } = require('../../../utils/session-token');
 const { passwordMatched } = require('../../../utils/password');
 
-/**
- * Check username and password for login.
- * @param {string} email - Email
- * @param {string} password - Password
- * @returns {object} An object containing, among others, the JWT token if the email and password are matched. Otherwise returns null.
- */
+const MAX_LOGIN_ATTEMPTS = 5;
+const LOGIN_RESET_TIME_MINUTES = 30;
+const loginAttempts = new Map();
+
 async function checkLoginCredentials(email, password) {
   const user = await authenticationRepository.getUserByEmail(email);
 
-  // We define default user password here as '<RANDOM_PASSWORD_FILTER>'
-  // to handle the case when the user login is invalid. We still want to
-  // check the password anyway, so that it prevents the attacker in
-  // guessing login credentials by looking at the processing time.
-  const userPassword = user ? user.password : '<RANDOM_PASSWORD_FILLER>';
+  if (loginAttempts.has(email)) {
+    const { attempts, loginTerakhir } = loginAttempts.get(email);
+    const now = Date.now();
+    const waktu = now - loginTerakhir;
+    const menitTunggu = Math.floor(waktu / (1000 * 60));
+
+    if (
+      attempts >= MAX_LOGIN_ATTEMPTS &&
+      menitTunggu < LOGIN_RESET_TIME_MINUTES
+    ) {
+      return {
+        attempts: 'Limit reached',
+        message:
+          'Attempts limit reached, try again in ' +
+          (30 - menitTunggu) +
+          ' minutes',
+      };
+    } else if (menitTunggu >= LOGIN_RESET_TIME_MINUTES) {
+      loginAttempts.delete(email);
+    }
+  }
+
+  const userPassword = user.password;
   const passwordChecked = await passwordMatched(password, userPassword);
 
-  // Because we always check the password (see above comment), we define the
-  // login attempt as successful when the `user` is found (by email) and
-  // the password matches.
   if (user && passwordChecked) {
+    loginAttempts.delete(email);
+
     return {
       email: user.email,
       name: user.name,
       user_id: user.id,
       token: generateToken(user.email, user.id),
     };
+  } else if (loginAttempts.has(email)) {
+    const attempts = loginAttempts.get(email).attempts + 1;
+    const loginTerakhir = Date.now();
+    loginAttempts.set(email, {
+      attempts,
+      loginTerakhir,
+    });
+  } else if (!loginAttempts.has(email)) {
+    const attempts = 1;
+    const loginTerakhir = Date.now();
+    loginAttempts.set(email, {
+      attempts,
+      loginTerakhir,
+    });
   }
-
   return null;
 }
 
-module.exports = {
-  checkLoginCredentials,
-};
+function checkDataPercobaan(email) {
+  const attempts = loginAttempts.get(email);
+
+  if (attempts) {
+    return attempts.attempts;
+  } else {
+    return 1;
+  }
+}
+
+module.exports = { checkLoginCredentials, checkDataPercobaan };
